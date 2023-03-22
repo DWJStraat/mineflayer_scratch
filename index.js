8a00cc1971824a4d9a32155f0bf9100808c5412e
Dstra <64135655+DWJStraat@users.noreply.github.com>
2023-03-22 11:49:48 +0100

    Fixes

--- index.js
+++ index.js
@@ -28,48 +28,16 @@ class ScratchMineflayer {
                         }
                     }
                 },
-                {
-                    "opcode": "jsonExtract",
-                    "blockType": "reporter",
-                    "text": "extract [name] from [data]",
-                    "arguments": {
-                        "name": {
-                            "type": "string",
-                            "defaultValue": "temperature"
-                        },
-                        "data": {
-                            "type": "string",
-                            "defaultValue": '{"temperature": 12.3}'
-                        },
-                    }
-                },
             ],
         };
     }
 
     connect(args) {
-        var bot = mineflayer.createBot({
+        let bot = mineflayer.createBot({
             host: args.HOST,
             port: args.port,
             username: args.USERNAME,
         })
-    }
-}
-
-    jsonExtract({name,data}) {
-        var parsed = JSON.parse(data)
-        if (name in parsed) {
-            var out = parsed[name]
-            var t = typeof(out)
-            if (t == "string" || t == "number")
-                return out
-            if (t == "boolean")
-                return t ? 1 : 0
-            return JSON.stringify(out)
-        }
-        else {
-            return ""
-        }
     }
 }
 
